8ceea54dfc16e0dea9ee1d59e4b796b9c381ee6f
KatarzynaKorbut <korbutkasia@gmail.com>
2021-12-10 14:00:03 +0100

    Stała (pozioma) orientacja ekranu

--- app/src/main/java/com/example/resistance_is_futile/MainActivity.java
+++ app/src/main/java/com/example/resistance_is_futile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.resistance_is_futile;
 
+import android.content.pm.ActivityInfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
