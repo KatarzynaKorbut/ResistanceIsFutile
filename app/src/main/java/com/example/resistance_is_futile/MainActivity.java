75b0403f21218212b34ff4456d0d5ebc2e5f1702
KatarzynaKorbut <korbutkasia@gmail.com>
2021-12-10 14:42:36 +0100

    Chowanie paska powiadomień oraz guzików nawigacyjnych

--- app/src/main/java/com/example/resistance_is_futile/MainActivity.java
+++ app/src/main/java/com/example/resistance_is_futile/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
