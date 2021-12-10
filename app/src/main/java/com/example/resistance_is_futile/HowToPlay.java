7bb21d20ac69b8d0c32b419dfaa558ae9746f3d1
KatarzynaKorbut <korbutkasia@gmail.com>
2021-12-10 17:04:34 +0100

    Przejścia między aktywnościami MainActivity <-> HowToPlay (włączenie zasad gry oraz powrót do menu)

--- app/src/main/java/com/example/resistance_is_futile/HowToPlay.java
+++ app/src/main/java/com/example/resistance_is_futile/HowToPlay.java
@@ -1,19 +1,20 @@
 package com.example.resistance_is_futile;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button_how_to_play;
+public class HowToPlay extends AppCompatActivity {
+    private ImageButton button_powrot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.how_to_play);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -23,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        button_how_to_play = (Button) findViewById(R.id.zasady);
-        button_how_to_play.setOnClickListener(v -> openHowToPlay());
+        button_powrot = (ImageButton) findViewById(R.id.powrot);
+        button_powrot.setOnClickListener(v -> openMainActivity());
     }
 
-    public void openHowToPlay() {
-        Intent intent = new Intent(this, HowToPlay.class);
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
