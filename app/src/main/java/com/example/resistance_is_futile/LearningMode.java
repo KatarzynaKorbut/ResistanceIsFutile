1a7fb3238bf250d7d9fe63d3bb09bcf364d481d1
KatarzynaKorbut <korbutkasia@gmail.com>
2022-01-05 10:50:00 +0100

    dodanie aktywności "Learning Mode" oraz guzika umożliwiającego przemieszczanie się pomiędzy trybem nauki oraz menu

--- app/src/main/java/com/example/resistance_is_futile/LearningMode.java
+++ app/src/main/java/com/example/resistance_is_futile/LearningMode.java
@@ -1,20 +1,20 @@
 package com.example.resistance_is_futile;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button_how_to_play;
-    private Button button_learning_mode;
+public class LearningMode extends AppCompatActivity {
+    private ImageButton button_powrot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.learning_mode);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -24,19 +24,12 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        button_how_to_play = findViewById(R.id.zasady);
-        button_how_to_play.setOnClickListener(v -> openHowToPlay());
-        button_learning_mode = findViewById(R.id.learning_mode);
-        button_learning_mode.setOnClickListener(v -> openLearningMode());
+        button_powrot = findViewById(R.id.powrot);
+        button_powrot.setOnClickListener(v -> openMainActivity());
     }
 
-    public void openHowToPlay() {
-        Intent intent = new Intent(this, HowToPlay.class);
-        startActivity(intent);
-    }
-
-    public void openLearningMode() {
-        Intent intent = new Intent(this, LearningMode.class);
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
