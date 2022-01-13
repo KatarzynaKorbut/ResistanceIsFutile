a3a7de72f4907a187bc7c6b4f519d9870b0d3e42
KatarzynaKorbut <korbutkasia@gmail.com>
2022-01-13 15:46:41 +0100

    Wybieranie koloru przyciskami

--- app/src/main/java/com/example/resistance_is_futile/NewGame.java
+++ app/src/main/java/com/example/resistance_is_futile/NewGame.java
@@ -6,12 +6,10 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class NewGame extends AppCompatActivity {
-    private ImageButton button_powrot;
-    private ImageButton button_pasek;
-
     public enum Bands {
         ZERO(R.color.res_black),
         ONE(R.color.res_brown),
@@ -50,9 +48,41 @@ public class NewGame extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        button_powrot = findViewById(R.id.powrot);
-        button_powrot.setOnClickListener(v -> openMainActivity());
+        findViewById(R.id.powrot).setOnClickListener(v -> openMainActivity());
+
+        findViewById(R.id.button1).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button2).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button3).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button4).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button5).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button6).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button7).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button8).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button9).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button10).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button11).setOnClickListener(v -> onColorButtonClicked(v));
+        findViewById(R.id.button12).setOnClickListener(v -> onColorButtonClicked(v));
     }
+
+    private int selectedColorButton = -1;
+
+    public void onColorButtonClicked(View v) {
+        int buttonId = v.getId();
+        int newSelectedButton = buttonId;
+        if (selectedColorButton != -1) {
+            Button previousColorButton = findViewById(selectedColorButton);
+            previousColorButton.setScaleX(1.0f);
+            previousColorButton.setScaleY(1.0f);
+            newSelectedButton = -1;
+        }
+        if (buttonId != selectedColorButton) {
+            v.setScaleX(0.8f);
+            v.setScaleY(0.8f);
+            newSelectedButton = buttonId;
+        }
+        selectedColorButton = newSelectedButton;
+    }
+
 
     public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
